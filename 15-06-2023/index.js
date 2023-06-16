8d904523b899b8ce2ba86d77df5e85d446ec7813
jeelrajpara <jeel.rajpara@saeculumsolutions.com>
2023-06-16 10:14:40 +0530

    commit

---
+++ 15-06-2023/index.js
@@ -0,0 +1,32 @@
+
+
+// var x=7;
+// function myName(){
+//     console.log("hello wordld");
+// }
+// myName();
+// console.log(x);
+
+
+// var n=2;
+// function square(num)
+// {
+//     var ans= (num*num);
+//     return ans;
+// }
+// var square2=square(n);
+// var square4=square(4);
+// console.log(square);
+// console.log(square2);
+// console.log(square4);
+
+
+
+// let a=12;
+// console.log(a);
+// var b=100;
+
+
+// 'use strict';
+// x=7;
+// console.log(x);
