b2e86b1d54b43435c8dbc1e553224d26fa6047ce
jeelrajpara <jeel.rajpara@saeculumsolutions.com>
2023-07-12 10:18:24 +0530

    commit

---
+++ 12-07-2023/number.js
@@ -0,0 +1,24 @@
+//.......................................parseInt()...................................//
+// console.log(parseInt("10")); //10
+// console.log(parseInt("12.45"));  //12
+// console.log(parseInt("12",8));  //10
+// console.log(parseInt("12 years")); //12
+// console.log(parseInt("years 12")); //NaN
+// console.log(parseInt("null"))  //NaN
+// console.log(parseInt("")); //NaN
+// console.log(parseInt("undefined")) //NaN
+// console.log(parseInt("12",8));  //10 converts 12 to decimal 
+// console.log(parseInt("12",16));  //18 converts 12 to hexadecimal
+// console.log(parseInt("0x12"));   //takes 12 ans converts it to hexadecimal
+// console.log(parseInt("010"));  //10
+// console.log(parseInt(" 90 "));  //90    
+
+
+//.......................................parseFloat().................................//
+// console.log(parseFloat("10")); //10
+// console.log(parseFloat("12.45")); //12.45
+// console.log(parseFloat("years 12")); //NaN
+// console.log(parseFloat("null")) //NaN
+// console.log(parseFloat("undefined")); //NaN
+// console.log(parseFloat(" 90 ")); //90
+// console.log(parseFloat("10.00")); //10
