53c6525ffa08dd594195b73ae441fcbf793b486e
jeelrajpara <jeel.rajpara@saeculumsolutions.com>
2023-07-14 14:07:05 +0530

    commit before lunch

--- 14-07-2023/script1.js
+++ 14-07-2023/script1.js
@@ -8,13 +8,38 @@
 // addElement('CSS');
 // addElement('Javascript')
 
-function addElement(langName){
-    let li = document.createElement('li')
-    li.innerHTML=`${langName}`
-    document.querySelector('.language').appendChild(li)
-}
-addElement('CSS');
-addElement('JavaScript')
+// function addElement(langName){
+//     let li = document.createElement('li')
+//     li.innerHTML=`${langName}`
+//     document.querySelector('.language').appendChild(li)
+// }
+// addElement('CSS');
+// addElement('JavaScript')
 
-//Edit values
-document.querySelector()
+// //Edit values
+// document.querySelector()
+
+// let arr1 = [
+//     {
+//         id: 1,
+//         slot: [
+//             { dosage: " " },
+//             { dosage: "hello" },
+//         ]
+//     },
+//     {
+//         id: 2,
+//         slot: [
+//             { dosage: "a" },
+//             { dosage: "b" },
+//         ]
+//     },
+// ]
+// function filterObject(arr1){
+//    let result = arr1.some(item=>{
+//       return item.slot.some(slot=> slot.dosage.trim()=="")
+//    });
+//    return result;
+// }
+// console.log(filterObject(arr1))
+
