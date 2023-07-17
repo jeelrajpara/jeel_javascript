07690bfa7249e19da1b07bdfc5fa3f3c9250f0db
jeelrajpara <jeel.rajpara@saeculumsolutions.com>
2023-07-17 12:45:09 +0530

    commit (11 AM)

---
+++ 17-07-2023/script.js
@@ -0,0 +1,5 @@
+let element = document.getElementById('div-03')
+// console.log(element.closest('#div-02'))
+// console.log(element.closest('#div-01')) //provided selector is empty
+// console.log(element.closest('div div'))
+console.log(element.closest('article>div'))
