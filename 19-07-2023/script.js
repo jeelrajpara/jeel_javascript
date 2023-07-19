92f12f129cef7ad79834cb2b2f11b5d0da7a5c29
jeelrajpara <jeel.rajpara@saeculumsolutions.com>
2023-07-19 13:10:46 +0530

    before lunch

--- 19-07-2023/script.js
+++ 19-07-2023/script.js
@@ -56,3 +56,93 @@
 // console.log(isNumber(["1",'a','h']))
 // console.log(isNumber(["a",3,'h']))
 
+// let arr1 = [1,230,322,45,90];
+// arr1.every((value)=>value>34 && value<250);
+// console.log(arr1);
+
+// let arr1 = [1,2,3,4,5]
+// let arr2 = arr1.fill(0,2,4)
+// console.log(arr1)
+// console.log(arr2)
+
+// var arr = new Array(5);
+// arr.fill(0);
+
+// console.log(arr);
+
+// var arr = [];
+// arr.fill(10, 0, 5);
+
+// console.log(arr);
+
+// var arr = [1, 3, 5, 7];
+// console.log(arr.slice(1));
+// let arr;
+// console.log(arr=arr.slice())
+
+// var nums = [1, 2, 3];
+// console.log(nums.map())
+
+// function compare(a, b) {
+//     if (a.year < b.year) return -1;
+//     if (a.year > b.year) return 1;
+
+//     if (a.name < b.name) return -1;
+//     if (a.name > b.name) return 1;
+//     return 0;
+// }
+
+// var langs = [
+//     {name: 'JavaScript', year: 1995},
+//     {name: 'Python', year: 1991},
+//     {name: 'Java', year: 1995},
+//     {name: 'C++', year: 1989}
+// ];
+
+// console.log(langs.sort(compare));
+
+// const products = [
+//     { name: "Product 1", price: 20, category: "Electronics" },
+//     { name: "Product 2", price: 30, category: "Clothes" },
+//     { name: "Product 3", price: 40, category: "Electronics" },
+//     { name: "Product 4", price: 50, category: "Clothes" },
+//     { name: "Product 5", price: 60, category: "Clothes" },
+//     { name: "Product 6", price: 70, category: "Electronics" },
+//     { name: "Product 7", price: 80, category: "Clothes" },
+//     { name: "Product 8", price: 90, category: "Electronics" },
+//     { name: "Product 8", price: 90, category: "xyz" },
+// ];
+// [a]
+// function getCategory(products){
+//     let result = products.reduce((accu,cur)=>{
+//         let category = cur.category;
+//         accu[category] = (accu[category]||0) + 1;
+//         return accu;
+//     },{})
+//     return result;
+// }
+// products.push({ name: "Product 8", price: 90, category: "xyz" })
+// console.log(getCategory(products));
+
+// [b]
+// function getAverage(products){
+//     let result = [];
+//         for (let product of products) {
+//           let category = product.category;
+//           let existingCategory = result.find(item => item.category === category);
+    
+//           if (existingCategory) {
+//             existingCategory.totalPrice += product.price;
+//             existingCategory.count++;
+//           } else {
+//             result.push({ category: category, totalPrice: product.price, count: 1 });
+//           }
+//         }
+//         for (let categoryObj of result) {
+//           categoryObj.average = categoryObj.totalPrice / categoryObj.count;
+//           delete categoryObj.totalPrice;
+//           delete categoryObj.count;
+//         }
+//         return result;
+// }
+// console.log(getAverage(products));
